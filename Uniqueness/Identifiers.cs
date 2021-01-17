21e0c479d7c1ee0b7aa208f727679345bca9057a
Bertie2011 <44732286+Bertie2011@users.noreply.github.com>
2021-01-17 19:19:59 +0100

    Update Shared.dll + Added additional subfolder config for ResourceLocation.

---
+++ Uniqueness/Identifiers.cs
@@ -0,0 +1,22 @@
+﻿using DataPackChecker.Shared;
+using DataPackChecker.Shared.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace Uniqueness {
+    public class Identifiers : CheckerRule {
+        public override string Title => "All in-game resources must be prefixed by a short identifier and a separator.";
+
+        public override string Description => "In order to prevent clashes with resources of other data packs, all in-game resources should be prefixed.\nPossible separators are: . _ -";
+
+        public override string GoodExample => "abc_my_objective AND abc_my_tag\nxyz.my.objective AND xyz.my.tag";
+
+        public override string BadExample => "someObjective\nabc_my_objective AND xyz_my_tag";
+
+        public override void Run(DataPack pack, JsonElement? config, Output output) {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Uniqueness/ResourceLocation.cs
+++ Uniqueness/ResourceLocation.cs
@@ -15,11 +15,26 @@ namespace Uniqueness {
 
         public override string Description => "Assuming the namespace is author specific, putting all resources in subfolders will prevent clashes with other data packs of the same author.";
 
-        public override string GoodExample => "data/<namespace>/functions/MyDataPack/name.mcfunction AND data/<namespace>/predicates/MyDataPack/name.json\n\tdata/<namespace>/functions/MyDataPack/name.mcfunction AND data/<namespace>/predicates/MyDataPack/AnotherFolder/name.json";
+        public override string GoodExample => "data/<namespace>/functions/MyDataPack/name.mcfunction AND data/<namespace>/predicates/MyDataPack/name.json\ndata/<namespace>/functions/MyDataPack/name.mcfunction AND data/<namespace>/predicates/MyDataPack/AnotherFolder/name.json";
 
-        public override string BadExample => "data/<namespace>/functions/name.mcfunction\n\tdata/<namespace>/functions/MyDataPack/name.mcfunction AND data/<namespace>/predicates/SomethingElse/name.json";
+        public override string BadExample => "data/<namespace>/functions/name.mcfunction\ndata/<namespace>/functions/MyDataPack/name.mcfunction AND data/<namespace>/predicates/SomethingElse/name.json";
 
-        public override void Run(DataPack pack, JsonElement config, Output output) {
+        public override string ConfigExample => @"Only one subfolder name per namespace is allowed without configuration.
+Otherwise, the allowed subfolders can be extended or overriden by supplying a configuration like this:
+{
+    ""options"": [
+        ""other subfolder"",
+        ""...""
+    ],
+    ""extend"": true
+}";
+
+        public override void Run(DataPack pack, JsonElement? config, Output output) {
+            if (!ValidateConfig(config)) {
+                output.Error(new InvalidDataException("Configuration is not valid, see '-i Uniqueness.ResourceLocation'."));
+                return;
+            }
+
             foreach (var ns in pack.Namespaces) {
                 if (ns.Name == "minecraft") continue;
 
@@ -45,17 +60,37 @@ namespace Uniqueness {
                     .Concat(ns.WorldGenData.NoiseSettings)
                     .Concat(ns.WorldGenData.ProcessorLists)
                     .Concat(ns.WorldGenData.TemplatePools);
+
                 foreach (var resource in resources) {
+                    var path = resource.Path.Split('/', '\\')[0];
+                    bool configMatch = config != null && config.Value.GetProperty("options").EnumerateArray().Any(v => v.GetString() == path);
+                    bool append = config == null || config.Value.GetProperty("extend").GetBoolean();
+
                     if (string.IsNullOrWhiteSpace(resource.Path)) output.Error(ns, resource, "Resource is not in a subfolder");
-                    else if (subfolder != null && resource.Path.Split('/', '\\')[0] != subfolder) {
-                        output.Error(ns, resource, $"Resource does not have the same subfolder as {output.GetResourceIdentifier(subfolderNS, subfolderR)}");
-                    } else {
-                        subfolder = resource.Path.Split('/', '\\')[0];
+                    else if (!FolderAllowed(path, subfolder, append, configMatch)) {
+                        if (subfolder != null) output.Error(ns, resource, $"Resource subfolder does not match {output.GetResourceIdentifier(subfolderNS, subfolderR)} or any of the ones in the config file.");
+                        else output.Error(ns, resource, $"Resource subfolder does not match any of the ones in the config file.");
+                    } else if (subfolder == null && append && !configMatch) {
+                        subfolder = path;
                         subfolderNS = ns;
                         subfolderR = resource;
                     }
                 }
             }
+        }
+
+        private bool ValidateConfig(JsonElement? config) {
+            return config == null ||
+                (config.Value.ValueKind == JsonValueKind.Object &&
+                    config.Value.TryGetProperty("options", out JsonElement options) &&
+                    options.EnumerateArray().All(v => v.ValueKind == JsonValueKind.String) &&
+                    config.Value.TryGetProperty("extend", out JsonElement extend) &&
+                    (extend.ValueKind == JsonValueKind.False || extend.ValueKind == JsonValueKind.True));
+        }
+
+        private bool FolderAllowed(string path, string subfolder, bool append, bool configMatch) {
+            return (subfolder == null && (append || configMatch))
+                || (subfolder != null && (subfolder == path || configMatch));
         }
     }
 }
