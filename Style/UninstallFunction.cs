2e137ebcce5e2a0363159d7bf3b142650e17cfa3
Bertie2011 <44732286+Bertie2011@users.noreply.github.com>
2021-01-20 10:29:47 +0100

    Save of Style project

---
+++ Style/UninstallFunction.cs
@@ -0,0 +1,20 @@
+﻿using DataPackChecker.Shared;
+using DataPackChecker.Shared.Data;
+using System;
+using System.Text.Json;
+
+namespace Style {
+    public class UninstallFunction : CheckerRule {
+        public override string Title => "There must be an uninstall function.";
+
+        public override string Description => "Providing an uninstall function will help remove traces and leave a clean world behind before the data pack is removed.";
+
+        public override string GoodExample => "Having an uninstall.mcfunction file somewhere.";
+
+        public override string BadExample => "Not having an uninstall function";
+
+        public override void Run(DataPack pack, JsonElement? config, Output output) {
+            throw new NotImplementedException();
+        }
+    }
+}
